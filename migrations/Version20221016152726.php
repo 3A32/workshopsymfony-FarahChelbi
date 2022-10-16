34f2eef6dea7b6b03afb7ebbc0b41e65cc15f3fc
chelbifarah <79860731+chelbifarah@users.noreply.github.com>
2022-10-16 22:30:54 +0100

    Atelier4

---
+++ migrations/Version20221016152726.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221016152726 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (ref VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(ref)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE classroom (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE club (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (ref INT AUTO_INCREMENT NOT NULL, title VARCHAR(250) NOT NULL, description LONGTEXT NOT NULL, price DOUBLE PRECISION DEFAULT NULL, PRIMARY KEY(ref)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE student (cne VARCHAR(10) NOT NULL, username VARCHAR(255) NOT NULL, PRIMARY KEY(cne)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE classroom');
+        $this->addSql('DROP TABLE club');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE student');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
---
+++ src/Controller/ClassroomController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Classroom;
+use App\Form\ClassroomType;
+use App\Repository\ClassroomRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClassroomController extends AbstractController
+{
+    #[Route('/classroom', name: 'app_classroom')]
+    public function index(): Response
+    {
+        return $this->render('classroom/index.html.twig', [
+            'controller_name' => 'ClassroomController',
+        ]);
+    }
+
+    #[Route('/classroomList', name: 'app_classroomList')]
+    public function list(ClassroomRepository $repository)
+    {
+        $classroom=$repository->findAll();
+        return $this->render("classroom/list.html.twig",array("tabClassrooms"=>$classroom));
+    }
+
+    #[Route('/addClassroom', name: 'app_addClassroom')]
+    public function addClassroom(ManagerRegistry $doctrine, Request $request)
+    {
+        $classroom=new Classroom();
+        $form=$this->createForm(ClassroomType::class,$classroom);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            $em=$doctrine->getManager();
+            $em->persist($classroom);
+            $em->flush();
+            return $this->redirectToRoute("app_classroomList");
+        }
+        return $this->renderForm("classroom/addClassroom.html.twig",
+         array("formClass"=>$form));
+
+         
+    }
+
+    #[Route('/updateClassroom/{id}', name: 'app_updateClassroom')]
+    public function updateClassroom(ClassroomRepository $repository, $id, ManagerRegistry $doctrine, Request $request)
+    {
+        $classroom=$repository->find($id);
+        $form = $this->createForm(ClassroomType::class, $classroom);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted()) { 
+            $em=$doctrine->getManager();
+            $em->flush();
+            return $this->redirectToRoute("app_ClassroomList");
+        }
+        return $this->renderForm("classroom/updateClassroom.html.twig",
+        array("formClass"=>$form));
+    }
+
+    #[Route('/removeClassroom/{id}', name: 'app_removeClassroom')]
+    public function removeClassroom(ManagerRegistry $doctrine, $id, ClassroomRepository $repository)
+    {
+        $classroom=$repository->find($id);
+        $em=$doctrine->getManager();
+        $em->remove($classroom);
+        $em->flush();
+        return $this->redirectToRoute("app_ClassroomList");
+    }
+
+
+}
+
+
